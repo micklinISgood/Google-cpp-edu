8cf7cff03c2a2421171d20726e1673efda259f6e
Mick Lin <cl3469@columbia.edu>
2016-12-27 21:31:51 -0500

    6 rows 17 spaces

--- temp.cpp
+++ temp.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-
+#include <iomanip>
 using namespace std;
 
 int main()
 {
-	for(int i =0; i <4; i++){
-		cout << "Hello World!" << endl;
+	for(int i =0; i <6; i++){
+		for(int j=0; j <4; j++)cout << setw(17)<<"Hello World!";
+		cout << endl;
 	}
 	return 0;
 }
