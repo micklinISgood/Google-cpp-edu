a701fd2a1309794daff563a015dd58adac362597
Mick Lin <cl3469@columbia.edu>
2016-12-27 23:17:03 -0500

    follow the official ans

--- guess.cpp
+++ guess.cpp
@@ -8,22 +8,22 @@ using namespace std;
 
 int main() {
   int iSecret;
-  int input_var = -1;
+  int guess;
   /* initialize random seed: */
   srand (time(NULL));
 
-  /* generate secret number between 1 and 10: */
-  iSecret = rand() % 99+1;
+  /* generate secret number between 1 and 100: */
+  iSecret = rand() % 100+1;
   // Enter the do while loop and stay there until either
   // a non-numeric is entered, or -1 is entered. Note that
   // cin will accept any integer, 4, 40, 400, etc.
   do {
-    cout << "Guess a number: ";
+    cout << "Guess our number (1 to 100) ";
     // The following line accepts input from the keyboard into
-    // variable input_var.
+    // variable guess.
     // cin returns false if an input operation fails, that is, if
-    // something other than an int (the type of input_var) is entered.
-    if (!(cin >> input_var)) {
+    // something other than an int (the type of guess) is entered.
+    if (!(cin >> guess)) {
       cout << "You entered a non-numeric. Exiting..." << endl;
       //break;
       cin.clear();
@@ -31,14 +31,13 @@ int main() {
       continue;
       // exit the do while loop
     }
-    if (input_var == iSecret ) {
-      cout << "Guessed! " << input_var << endl;
-      break;
-    }else if(input_var < iSecret) cout << "Guessed number is lower than secret"<<endl;
-    else cout <<  "Guessed number is higher than secret"<<endl;
+    
+   
+    if(guess < iSecret) cout << "The secret number is higher than "<<guess<<endl;
+    else if(guess >iSecret) cout <<  "The secret number is lower than "<<guess<<endl;
 
 
-  } while (input_var != -1);
-  cout << "All done." << endl;
+  } while (iSecret != guess);
+  cout << "Congratulations!" << endl;
   return 0;
 }
