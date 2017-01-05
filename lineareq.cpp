6d21879511454ab3c3afa977f52db78edbe2cd80
Mick Lin <cl3469@columbia.edu>
2017-01-05 14:27:51 +0800

    add google sol

--- lineareq.cpp
+++ lineareq.cpp
@@ -31,4 +31,14 @@ int main()
 	}
 	return 0;
 }
+int googleSol () {
+
+  for (int h = 0; h < 100; h++)
+    for (int p = 0; p < 100; p++)
+      for (int r = 0; r < 100; r++)
+        if ((h + p + r) == 100)
+          if (((10 * h) + (3 * p) + (0.5 * r)) == 100)
+            cout << "Found one! " << h << " horses " << p << " pigs " << r << " rabbits "  << endl;
+   return 0;
+}
    
